96474e67a1d5c7129ae2e6029fa68c87cc675e69
Evandro Luis <evandroluis58@gmail.com>
2018-01-03 23:43:10 -0300

    Hello world implement

--- Hello_World.py
+++ Hello_World.py
@@ -2,5 +2,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World! '
+def projects():
+    return 'index'
+
+@app.route('/1')
+def about():
+    return 'pagina 1'
+
+@app.route('/user/<username>')
+def profile(username):
+    return 'Hello '+ username
